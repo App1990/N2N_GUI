3ba89fb25a0801609044c62bbb80fd7cb5d067ac
wwb <840172959@qq.com>
2020-03-02 19:54:23 +0800

    添加项目文件。

---
+++ N2N_GUI/N2N_GUI.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace N2N_GUI
+{
+    partial class N2N_GUI
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(N2N_GUI));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt__remote_port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt__remote_ip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt__assigned_ip = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt__password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt__group = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn__start = new System.Windows.Forms.Button();
+            this.btn__abort = new System.Windows.Forms.Button();
+            this.lst__log = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt__remote_port);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt__remote_ip);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Supernode";
+            // 
+            // txt__remote_port
+            // 
+            this.txt__remote_port.Location = new System.Drawing.Point(214, 57);
+            this.txt__remote_port.Name = "txt__remote_port";
+            this.txt__remote_port.Size = new System.Drawing.Size(53, 21);
+            this.txt__remote_port.TabIndex = 3;
+            this.txt__remote_port.Text = "10001";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txt__remote_ip
+            // 
+            this.txt__remote_ip.Location = new System.Drawing.Point(16, 57);
+            this.txt__remote_ip.Name = "txt__remote_ip";
+            this.txt__remote_ip.Size = new System.Drawing.Size(151, 21);
+            this.txt__remote_ip.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt__assigned_ip);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txt__password);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txt__group);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(13, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 152);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Edge(Local Settings)";
+            // 
+            // txt__assigned_ip
+            // 
+            this.txt__assigned_ip.Location = new System.Drawing.Point(123, 113);
+            this.txt__assigned_ip.Name = "txt__assigned_ip";
+            this.txt__assigned_ip.Size = new System.Drawing.Size(143, 21);
+            this.txt__assigned_ip.TabIndex = 5;
+            this.txt__assigned_ip.Text = "10.0.0.101";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Assigned IP";
+            // 
+            // txt__password
+            // 
+            this.txt__password.Location = new System.Drawing.Point(123, 74);
+            this.txt__password.Name = "txt__password";
+            this.txt__password.PasswordChar = '*';
+            this.txt__password.Size = new System.Drawing.Size(143, 21);
+            this.txt__password.TabIndex = 3;
+            this.txt__password.Text = "p@ssw0rd";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Password";
+            // 
+            // txt__group
+            // 
+            this.txt__group.Location = new System.Drawing.Point(123, 37);
+            this.txt__group.Name = "txt__group";
+            this.txt__group.Size = new System.Drawing.Size(143, 21);
+            this.txt__group.TabIndex = 1;
+            this.txt__group.Text = "WeGame";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Group";
+            // 
+            // btn__start
+            // 
+            this.btn__start.Location = new System.Drawing.Point(136, 315);
+            this.btn__start.Name = "btn__start";
+            this.btn__start.Size = new System.Drawing.Size(75, 23);
+            this.btn__start.TabIndex = 2;
+            this.btn__start.Text = "Start";
+            this.btn__start.UseVisualStyleBackColor = true;
+            this.btn__start.Click += new System.EventHandler(this.btn__start_Click);
+            // 
+            // btn__abort
+            // 
+            this.btn__abort.Location = new System.Drawing.Point(230, 315);
+            this.btn__abort.Name = "btn__abort";
+            this.btn__abort.Size = new System.Drawing.Size(75, 23);
+            this.btn__abort.TabIndex = 3;
+            this.btn__abort.Text = "Abort";
+            this.btn__abort.UseVisualStyleBackColor = true;
+            this.btn__abort.Click += new System.EventHandler(this.btn__abort_Click);
+            // 
+            // lst__log
+            // 
+            this.lst__log.FormattingEnabled = true;
+            this.lst__log.ItemHeight = 12;
+            this.lst__log.Location = new System.Drawing.Point(320, 19);
+            this.lst__log.Name = "lst__log";
+            this.lst__log.Size = new System.Drawing.Size(355, 280);
+            this.lst__log.TabIndex = 4;
+            // 
+            // N2N_GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 354);
+            this.Controls.Add(this.lst__log);
+            this.Controls.Add(this.btn__abort);
+            this.Controls.Add(this.btn__start);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "N2N_GUI";
+            this.Text = "N2N GUI";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.N2N_GUI_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt__remote_ip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt__remote_port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt__group;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt__password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt__assigned_ip;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn__start;
+        private System.Windows.Forms.Button btn__abort;
+        private System.Windows.Forms.ListBox lst__log;
+    }
+}
+
---
+++ N2N_GUI/N2N_GUI.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace N2N_GUI
+{
+    public partial class N2N_GUI : Form
+    {
+        Process p;
+
+        public N2N_GUI()
+        {
+            InitializeComponent();
+
+            btn__start.Enabled = true;
+            btn__abort.Enabled = false;
+            string strFileFullName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "N2N_GUI.ini");
+
+            //<CONFIG_DATA><N2N_GUI_SETUP REMOTE_IP="106.15.40.169" REMOTE_PORT="10001" GROUP="WeGame" PASSWORD="p@ssw0rd" ASSIGNED_IP="10.0.0.101"></N2N_GUI_SETUP></CONFIG_DATA>
+
+            if (File.Exists(strFileFullName))
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(File.ReadAllText(strFileFullName));
+
+                txt__remote_ip.Text = xmlDoc.SelectSingleNode("//@REMOTE_IP").Value;
+                txt__remote_port.Text = xmlDoc.SelectSingleNode("//@REMOTE_PORT").Value;
+
+                txt__group.Text = xmlDoc.SelectSingleNode("//@GROUP").Value;
+                txt__password.Text = xmlDoc.SelectSingleNode("//@PASSWORD").Value;
+                txt__assigned_ip.Text = xmlDoc.SelectSingleNode("//@ASSIGNED_IP").Value;
+            }
+        }
+
+        private void btn__start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 保存配置信息
+                string strRemoteIP = txt__remote_ip.Text;
+                string strRemotePort = txt__remote_port.Text;
+
+                string strGroup = txt__group.Text;
+                string strPassword = txt__password.Text;
+                string strAssignedIp = txt__assigned_ip.Text;
+                string strXml = string.Format("<CONFIG_DATA><N2N_GUI_SETUP REMOTE_IP=\"{0}\" REMOTE_PORT=\"{1}\" GROUP=\"{2}\" PASSWORD=\"{3}\" ASSIGNED_IP=\"{4}\"></N2N_GUI_SETUP></CONFIG_DATA>"
+                        , strRemoteIP, strRemotePort, strGroup, strPassword, strAssignedIp);
+
+                string strFileFullName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "N2N_GUI.ini");
+                File.WriteAllText(strFileFullName, strXml);
+
+                CheckForIllegalCrossThreadCalls = false;
+
+                p = new Process();
+
+                p.StartInfo.FileName = "edge_v2.exe";
+                p.StartInfo.WorkingDirectory = Application.StartupPath;
+                p.StartInfo.Arguments = string.Format("-a {0} -c {1} -k {2} -l {3}:{4}", strAssignedIp, strGroup, strPassword, strRemoteIP, strRemotePort);//传递的参数
+
+                p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+                p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+                p.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+
+                p.Start();
+                p.BeginOutputReadLine();
+
+                btn__abort.Enabled = true;
+                btn__start.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(outLine.Data))
+                {
+                    lst__log.Items.Add(outLine.Data);
+                    lst__log.TopIndex = lst__log.Items.Count - lst__log.Height / lst__log.ItemHeight;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn__abort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (p != null)
+                {
+                    p.Kill();
+                    p.Close();
+                }
+
+                btn__start.Enabled = true;
+                btn__abort.Enabled = false;
+                lst__log.Items.Add(string.Format("{0} Abort!", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (p != null) p.Dispose();
+            }
+        }
+
+        private void N2N_GUI_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (p != null && btn__abort.Enabled)
+            {
+                p.Kill();
+                p.Close();
+                p.Dispose();
+            }
+        }
+    }
+}
